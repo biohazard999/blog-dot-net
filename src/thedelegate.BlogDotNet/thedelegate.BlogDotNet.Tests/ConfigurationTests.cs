3f277b8c87bff0567b768e072c73eae635928caa
m.grundner <m.grundner@delegate.at>
2013-08-23 17:16:15 +0200

    added configuration, fixed Render_FirstPost_2013 test

---
+++ src/thedelegate.BlogDotNet/thedelegate.BlogDotNet.Tests/ConfigurationTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+// ReSharper disable SuggestUseVarKeywordEvident
+
+namespace thedelegate.BlogDotNet.Tests
+{
+    [TestClass]
+    public class ConfigurationTests
+    {
+        [TestMethod]
+        public void Configuration_Implements_IConfiguration()
+        {
+            IConfiguration configuration = new Configuration();
+
+            configuration.Should().BeAssignableTo<IConfiguration>();
+        }
+
+        [TestMethod]
+        public void ContentLocation_ToSupportConvention_ShouldReturnContentDashBlog()
+        {
+            IConfiguration configuration = new Configuration();
+
+            configuration.ContentLocation.Should().Be("Content/blog");
+        }
+
+        [TestMethod]
+        public void ContentLocation_ToSupportConfiguration_ShouldBeSettable()
+        {
+            string expected = new Fixture().Create<string>();
+
+            IConfiguration configuration = new Configuration()
+            {
+                ContentLocation = expected
+            };
+
+            configuration.ContentLocation.Should().Be(expected);
+        }
+    }
+}
---
+++ src/thedelegate.BlogDotNet/thedelegate.BlogDotNet/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace thedelegate.BlogDotNet
+{
+    public class Configuration : IConfiguration
+    {
+        public Configuration()
+        {
+            ContentLocation = "Content/blog";
+        }
+
+        public string ContentLocation { get; set; }
+    }
+}
